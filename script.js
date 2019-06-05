c6f26e80cf35e19b4056221e0fd37fe86a655f7e
Sergey Dmitriev <serge.dmitriev@gmail.com>
2019-06-05 12:10:14 +0700

    get creds from localstorage

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
+var password = localStorage.getItem('password');
+var wsServers = localStorage.getItem('wsServers');
+var uri = localStorage.getItem('uri');
+
+console.log('uri', uri);
+console.log('password', password);
+console.log('wsServers', wsServers);
+
 var options = {
     media: {
       local: {
-        video: document.getElementById('localVideo')
+        video: false, // document.getElementById('localVideo'),
+        audio: document.getElementById('localVideo')
       },
       remote: {
-        video: document.getElementById('remoteVideo'),
+        video: false, // document.getElementById('remoteVideo'),
         // This is necessary to do an audio/video call as opposed to just a video call
         audio: document.getElementById('remoteVideo')
       }
     },
-    ua: {}
+    ua: {
+        uri: uri,
+        wsServers: [wsServers],
+        password: password,
+    }
   };
+
+console.log('options', options);
 
 var simple = new SIP.Web.Simple(options);
 
@@ -22,7 +37,7 @@ endButton.addEventListener("click", function () {
 
 simple.on('ringing', function() {
     simple.answer();
-});
+  });
 
 //makes the call
 simple.call('welcome@onsip.com');
